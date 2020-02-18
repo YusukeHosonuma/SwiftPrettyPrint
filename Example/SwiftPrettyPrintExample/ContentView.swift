50451b9871db4ac58025086f1f4896fd3cfb3244
Yusuke Hosonuma <tobi462@gmail.com>
2020-02-18 09:53:41 +0900

    init: Example app

---
+++ Example/SwiftPrettyPrintExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPrettyPrintExample
+//
+//  Created by Yusuke Hosonuma on 2020/02/18.
+//  Copyright © 2020 Yusuke Hosonuma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
