b9f5a831998b425d0249fc53bb2260f7f265b0d3
sahara-ooga <performa532@gmail.com>
2020-03-15 14:07:32 +0900

    fix to remove leaked unnecessary judge
    Co-Authored-By: Yusuke Hosonuma <tobi462@gmail.com>

--- Sources/Core/Formatter/MultilineFormatter.swift
+++ Sources/Core/Formatter/MultilineFormatter.swift
@@ -33,10 +33,8 @@ class MultilineFormatter: PrettyFormatter {
     }
 
     func tupleString(elements: [(String?, String)]) -> String {
-        // if the labels of tuples are not specificated, they are like ".1" (not nil).
-        // Using "." as the first charactor of the label of tuple is prohibited.
         let labelValuePairs: [String] = elements.map { label, value in
-            if let label = label, label.first != "." {
+            if let label = label {
                 return label + ": " + value.indentTail(size: "\(label): ".count)
             } else {
                 return value
